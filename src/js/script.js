c1fe39a4b3329a9744bc9a02bfe0b9fc6d77e256
taji <taji@lucky-brothers.co.jp>
2016-09-09 15:24:41 +0900

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,58 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+// cameraの位置
+/*
+camera.position.y = 1.5;
+camera.position.z = 5;
+*/
+
+// レンダラー
+/*
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+*/
+
+// 物体
+/*
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshLambertMaterial( { color: 0x00ff88 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+*/
+
+// ライト
+/*
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(1, 1, 1).normalize();
+scene.add( light );
+*/
+
+// 描画のルーティン
+function render(t) {
+	requestAnimationFrame(render);
+
+    // 物体の回転
+    /*
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    */
+
+    // カメラの回転
+    /*
+    camera.position.x = Math.sin(t / 1000) * 5;
+    camera.position.z = Math.cos(t / 1000) * 5;
+    */
+
+    // カメラの向き
+    /*
+    camera.lookAt({x: 0, y: 0, z: 0});
+    light.position.x = Math.sin(t / 1000) * 5;
+    light.position.z = Math.cos(t / 1000) * 5;
+    */
+
+    // 描画
+	renderer.render(scene, camera);
+}
+render();
